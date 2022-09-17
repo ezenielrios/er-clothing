4b5f19767f44a94dfd0b2b5fffd9ad238d72b7e3
ezenielrios <ezeniel.rios.er@gmail.com>
2022-09-17 15:49:30 -0400

    added placeholder div for image in containers

--- er-clothing/src/App.js
+++ er-clothing/src/App.js
@@ -26,7 +26,7 @@ const App = () => {
     <div className='categories-container'>
       {categories.map(({title}) => (
         <div className='catagory-container'>
-          {/* <img /> */}
+          <div className='background-image' />
           <div className='category-body-container'>
             <h2>{title}</h2>
             <p>Shop Now</p>
