170a193926d2e89a8f88b1cd3405d1967c064c86
ezenielrios <ezeniel.rios.er@gmail.com>
2022-10-01 15:59:03 -0400

    user context/sign out

--- er-clothing/src/components/routes/navigation/navigation.component.jsx
+++ er-clothing/src/components/routes/navigation/navigation.component.jsx
@@ -5,10 +5,15 @@ import { ReactComponent as ErLogo} from '../../../assets/erClothing.svg'
 import './navigation.styles.scss';
 import { UserContext } from '../../../contexts/user.context';
 
-const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
-    console.log(currentUser);
+import { signOutUser } from '../../../utils/firebase/firebase.utils';
 
+const Navigation = () => {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const signOutHandler = async () => {
+        await signOutUser();
+        setCurrentUser(null);
+    }
     return (
         <Fragment>
             <div className='navigation'>
@@ -19,9 +24,14 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/auth'>
-                        SIGN IN
-                    </Link>
+                    {currentUser ? (
+                        <span className='nav-link' onClick={signOutHandler}>
+                            SIGN OUT</span>
+                    ): (
+                        <Link className='nav-link' to='/auth'>
+                          SIGN IN
+                        </Link> 
+                    )}  
                 </div>
             </div>
             <Outlet />
--- er-clothing/src/utils/firebase/firebase.utils.js
+++ er-clothing/src/utils/firebase/firebase.utils.js
@@ -5,7 +5,8 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
+    signInWithEmailAndPassword,
+    signOut
  } from 'firebase/auth';
  import {
     getFirestore,
@@ -70,10 +71,12 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
-}
+};
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
     if (!email || !password) return;
 
     return await signInWithEmailAndPassword(auth, email, password);
-}
+};
+
+export const signOutUser = async () => await signOut(auth);
