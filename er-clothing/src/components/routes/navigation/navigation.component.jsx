4a32186ac2608e4a885d45f4a8ffdcd58ab9b842
ezenielrios <ezeniel.rios.er@gmail.com>
2022-10-02 12:57:52 -0400

    added cart context/shopping icon/cart dropdown

--- er-clothing/src/components/routes/navigation/navigation.component.jsx
+++ er-clothing/src/components/routes/navigation/navigation.component.jsx
@@ -1,14 +1,20 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as ErLogo} from '../../../assets/erClothing.svg'
-import './navigation.styles.scss';
-import { UserContext } from '../../../contexts/user.context';
+import CartIcon from '../../cart-icon/cart-icon.component';
+import CartDropdown from '../../cart-dropdown/cart-dropdown.component';
 
+import { UserContext } from '../../../contexts/user.context';
+import { CartContext } from '../../../contexts/cart.context';
+
+import { ReactComponent as ErLogo} from '../../../assets/erClothing.svg'
 import { signOutUser } from '../../../utils/firebase/firebase.utils';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -28,7 +34,9 @@ const Navigation = () => {
                           SIGN IN
                         </Link> 
                     )}  
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
