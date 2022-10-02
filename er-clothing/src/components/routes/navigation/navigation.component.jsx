ca22cd757b653870ffb8b6492d10ef68d9412379
ezenielrios <ezeniel.rios.er@gmail.com>
2022-10-01 21:00:32 -0400

    finalizing auth listener

--- er-clothing/src/components/routes/navigation/navigation.component.jsx
+++ er-clothing/src/components/routes/navigation/navigation.component.jsx
@@ -8,12 +8,8 @@ import { UserContext } from '../../../contexts/user.context';
 import { signOutUser } from '../../../utils/firebase/firebase.utils';
 
 const Navigation = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const { currentUser } = useContext(UserContext);
 
-    const signOutHandler = async () => {
-        await signOutUser();
-        setCurrentUser(null);
-    }
     return (
         <Fragment>
             <div className='navigation'>
@@ -25,7 +21,7 @@ const Navigation = () => {
                         SHOP
                     </Link>
                     {currentUser ? (
-                        <span className='nav-link' onClick={signOutHandler}>
+                        <span className='nav-link' onClick={signOutUser}>
                             SIGN OUT</span>
                     ): (
                         <Link className='nav-link' to='/auth'>
