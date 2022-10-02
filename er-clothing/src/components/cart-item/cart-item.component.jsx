e3dfa09c015e0a8ed8708d782b3aa1a3cdbaa3c3
ezenielrios <ezeniel.rios.er@gmail.com>
2022-10-02 17:00:23 -0400

    addToCart feature

--- er-clothing/src/components/cart-item/cart-item.component.jsx
+++ er-clothing/src/components/cart-item/cart-item.component.jsx
@@ -1,13 +1,18 @@
 import './cart-item.styles.scss';
 
 const CartItem = ({CartItem}) => {
-    const { name, quantity } = CartItem;
+    const { name, imageUrl, price, quantity } = CartItem;
     return(
-        <div>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={`${name}`} />
+            <div className='item-detail'>
+                <span className='name'>{name}</span>
+                <span className='price'>
+                    {quantity} x ${price}
+                </span>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default CartItem;
