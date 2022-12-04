17aaa1c1ba49dd23c57b7e97220401cf164e6ba0
ezeniel rios <ezeniel.rios.er@gmail.com>
2022-12-04 16:51:27 -0500

    converted button and cart-dropdown component to styled components

--- er-clothing/src/components/button/button.component.jsx
+++ er-clothing/src/components/button/button.component.jsx
@@ -11,8 +11,7 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
         [BUTTON_TYPE_CLASSES.base]: BaseButton,
         [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-    }[buttonType]
-)
+    }[buttonType]);
 
 const Button = ({children, buttonType, ...otherProps }) => {
     const CustomButton = getButton(buttonType);
